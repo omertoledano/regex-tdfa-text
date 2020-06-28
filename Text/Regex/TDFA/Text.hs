97f031dded649bae61112f62a18d9bdb55f3ca1e
Omer Toledano <omert@fb.com>
2020-06-28 11:14:15 -0400

    Remove another instance

--- Text/Regex/TDFA/Text.hs
+++ Text/Regex/TDFA/Text.hs
@@ -38,8 +38,8 @@ import Text.Regex.TDFA.NewDFA.Uncons(Uncons(uncons))
 import qualified Text.Regex.TDFA.NewDFA.Engine as Engine(execMatch)
 import qualified Text.Regex.TDFA.NewDFA.Tester as Tester(matchTest)
 
-instance Extract T.Text where
-  before = T.take; after = T.drop; empty = T.empty
+-- instance Extract T.Text where
+--   before = T.take; after = T.drop; empty = T.empty
 
 -- instance Uncons T.Text where
 --   {- INLINE uncons #-}
